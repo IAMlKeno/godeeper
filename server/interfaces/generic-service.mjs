d90b94176a234b6de18f274cba9e0902a5cdb4a0
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-02-06 22:13:37 -0400

    feat: complete crud compatibility for tags

--- server/interfaces/generic-service.mjs
+++ server/interfaces/generic-service.mjs
@@ -1,25 +1,69 @@
+import { ObjectId } from "mongodb";
 import db from "../db/conn.mjs";
 
 class GenericService {
 
   constructor(table) {
     this.table = table;
+    this.collection = null;
   }
 
-  async getAll() {
-    let collection = db.collection(this.table);
-    let results = collection.find({}).toArray();
+  async getCollection() {
+    this.collection = await db.collection(this.table);
+  }
+
+  async getOne(id) {
+    let query = {_id: ObjectId.createFromHexString(id)};
+    await this.getCollection();
+    let result = await this.collection.findOne(query);
+
+    return result; // can be null.
+  }
+
+  /**
+   * Get all documents that match given criteria.
+   *
+   * @returns An array of results or empty array.
+   *
+   * TODO: accept filter and sorting arguments.
+   */
+  async getAll(query = {}) {
+    await this.getCollection();
+    let results = this.collection.find(query).toArray();
 
     return results;
   }
 
   async create(body) {
-    let collection = await db.collection(this.table);
-    let newDocument = body;
-    let result = await collection.insertOne(newDocument);
+    await this.getCollection();
+    let result = await this.collection.insertOne(body);
 
     return result;
   }
+
+  /**
+   * Update a document.
+   *
+   * @param {*} id the object identifier
+   * @param {Object} body A mongodb compliant object to use to update the given row
+   * @returns Array
+   */
+  async update(id, body) {
+    const query = { _id: ObjectId.createFromHexString(id) };
+    await this.getCollection();
+    let result = await this.collection.updateOne(query, body);
+
+    return result;
+  };
+
+  async delete(id) {
+    const query = { _id: ObjectId.createFromHexString(id) };
+
+    await this.getCollection();
+    let result = await this.collection.deleteOne(query);
+
+    return result;
+  };
 
 }
 
--- server/routes/tags.mjs
+++ server/routes/tags.mjs
@@ -1,6 +1,6 @@
 import express from "express";
-import db from "../db/conn.mjs";
 import GenericService from "../interfaces/generic-service.mjs";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 const table = 'tags';
@@ -8,36 +8,54 @@ const service = new GenericService(table);
 
 // Tags
 router.get('/', async (req, res) => {
-  console.log('STARTING');
   let results = await service.getAll();
-  console.log(results);
 
   res.send(results).status(200);
 });
 
 router.get('/:id', async (req, res) => {
-  console.log('STARTING');
-  let collection = await db.collection('tags');
-  console.log('STARTING 1');
-  console.log(collection);
-  let results = await collection.find({}).limit(50).toArray();
-  console.log('STARTING 2');
-  console.log(results);
+  let results = await service.getOne(req.params.id);
 
   res.send(results).status(200);
 });
-
 
 router.post('/', async (req, res) => {
   let result = await service.create(req.body);
   res.send(result).status(204);
 });
 
-router.put('/:id', (req, res) => res.send('Hello world!'))
-router.delete('/:id', (req, res) => res.send('Hello world!'))
+router.put('/:id', async (req, res) => {
+  let json = req.body;
+  const body = {
+    $set: {
+      name: json.name,
+    }
+  };
+  let result = await service.update(req.params.id, body);
+  res.send(result).status(204);
+});
+
+router.delete('/:id', async (req, res) => {
+  let results = await service.delete(req.params.id);
+  res.send(results).status(204);
+});
+
 /**
  * Accept an array of ids and return all matching tags
  */
-router.post('/set', (req, res) => res.send('Hello world!'))
+router.post('/set', async (req, res) => {
+  let inBody = [];
+  let json = req.body; // an array of objects { id: <value> }
+  json.forEach((i) => inBody.push(ObjectId.createFromHexString(i.id)));
+
+  const queryAndFilters = {
+    _id: {
+      $in: inBody
+    }
+  };
+  const results = await service.getAll(queryAndFilters);
+
+  res.send(results).status(200);
+});
 
 export default router;
